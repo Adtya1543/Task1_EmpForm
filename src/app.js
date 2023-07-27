2339daea4e1b2cf3e5c222211c0eda40e02a381f
Aditya1543 <f20191543@hyderabad.bits-pilani.ac.in>
2023-07-27 14:24:57 +0530

    First Commit

---
+++ src/app.js
@@ -0,0 +1,63 @@
+const express = require("express");
+const path = require("path");
+const cors = require("cors");
+const app = express();
+const PORT = 9000;
+require("./db/conn");
+
+const Form = require("./models/form");
+const bodyParser = require("body-parser");
+
+const template_path = path.join(__dirname, "../templates/views");
+
+app.set("view engine", "ejs");
+app.set("views", template_path);
+
+app.use(cors());
+app.use(bodyParser.json());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+    res.render("index");
+});
+app.get("/submit", (req,res) => {
+    res.render("index");
+})
+app.post("/submit", async (req,res) => {
+    try{
+        // userExist = await UserActivation.findOne({emailAddress : emailAddress});
+        // if(userExist){
+        //     return res.status(422).json({error : "Email Already exists"});
+        // }        
+
+        const { fullname, emailAddress, gender, phoneNumber, permanentAddress,currentAddress,dateOfJoining,jobTitle,department,salary,aadharNumber,panNumber,experience } = req.body;
+
+        const employeeData = new Form({
+            fullname,
+            emailAddress, 
+            gender,
+            phoneNumber,
+            permanentAddress,
+            currentAddress,
+            dateOfJoining,
+            jobTitle,
+            department,
+            salary,
+            // photo : req.body["photo"],
+            // sign : req.body["sign"],
+            aadharNumber,
+            panNumber,
+            experience
+        })
+
+      const datastored = await employeeData.save();
+      res.status(201).redirect("/");
+
+    }catch(error) {
+        res.status(400).send(error);
+    }
+})
+
+app.listen(PORT,() => {
+    console.log(`Server running on ${PORT}`);
+});
---
+++ src/models/form.js
@@ -0,0 +1,60 @@
+const mongoose = require("mongoose");
+
+const employeeSchema =  new mongoose.Schema({
+    fullname : {
+        type: String,
+        // required: true
+    },
+    emailAddress : {
+        type: String,
+        // required: true,
+        // unique: true
+    },
+    phoneNumber : {
+        type: Number,
+        // required: true,
+        // unique: true
+    },
+    permanentaddress :{
+        type: String,
+    },
+    currentAddress :{
+        type: String
+    },
+    dateOfJoining : {
+        type: Number
+    },
+    jobTitle : {
+        type:String,
+        // required: true
+    },
+    department : {
+        type:String,
+        // required: true
+    },
+    salary : {
+        type:Number
+    },
+    //photo upload?
+    aadharNumber : {
+        type: Number
+    },
+    panNumber : {
+        type: String,
+        // required: true 
+    },
+    experience : {
+        type: Number,
+        // required: true
+    }
+})
+
+const Form = new mongoose.model("Form",employeeSchema);
+
+// data = {
+// //     name : "adi"
+// // }
+// // Form.insertMany([data])
+// the commented code up is to  test data is visible or not 
+
+module.exports = Form;
